ab9202a802612310b95c8faaf2db8d8b2e0e2c41
joker-N1 <1248621324@qq.com>
2022-04-22 11:09:57 +0800

    0421_1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.31f7e1300bf00669bff2f3fddf9c4c66.js"
+  "precache-manifest.05ef6774890291e11ae527f26420067f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "real-qingshui-pass"});
